79f0e301a98926885616b149a3990ecf082ba8c1
prabhakar786-create <prabhakar786@gmail.com>
2026-03-09 21:06:33 +0530

    Create ride.js

---
+++ ride.js
@@ -0,0 +1,18 @@
+function bookRide(){
+
+let pickup=document.getElementById("pickup").value
+let drop=document.getElementById("drop").value
+let vehicle=document.getElementById("vehicle").value
+
+db.ref("rides").push({
+
+pickup:pickup,
+drop:drop,
+vehicle:vehicle,
+status:"waiting"
+
+})
+
+alert("Ride Requested")
+
+}
