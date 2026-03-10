5841d8f9046c273f8ac214eda46f0b8d1ec28e82
prabhakar786-create <prabhakar786@gmail.com>
2026-03-10 13:09:09 +0530

    Update ride.js

--- ride.js
+++ ride.js
@@ -3,6 +3,7 @@ function bookRide(){
 let pickup=document.getElementById("pickup").value
 let drop=document.getElementById("drop").value
 let vehicle=document.getElementById("vehicle").value
+const db=firebase.database();
 
 db.ref("rides").push({
 
